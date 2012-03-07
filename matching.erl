fc35fc66ce7c4a2579aa91e72589aa30d6c380cf
Nathan Wise <nathanwise@gmail.com>
2012-03-06 21:24:54 -0500

    Pattern Matching

---
+++ matching.erl
@@ -0,0 +1,6 @@
+Person = {person, {name, "Agent Smith"}, {profession, "Killing Programs"}}.
+{person, {name, Name}, {profession, Profession }} = Person.
+
+[Head | Tail] = [1,2,3].
+
+[One, Two | Rest] = [1,2,3].
