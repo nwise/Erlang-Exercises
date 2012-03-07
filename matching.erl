d34e50a5983f24befc6a9c039ce4f3130f6d0576
Nathan Wise <nathanwise@gmail.com>
2012-03-06 21:34:34 -0500

    Adding function calls.

--- matching.erl
+++ matching.erl
@@ -1,6 +1,13 @@
+% Matching
 Person = {person, {name, "Agent Smith"}, {profession, "Killing Programs"}}.
 {person, {name, Name}, {profession, Profession }} = Person.
 
 [Head | Tail] = [1,2,3].
 
 [One, Two | Rest] = [1,2,3].
+
+% Functions
+
+c(basic).
+basic:mirror(smiling_mug).
+basic:mirror(1)
